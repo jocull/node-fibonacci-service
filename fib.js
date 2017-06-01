7378e333a64ecfb9c6d82276cbf3df28d8d5956c
James O'Cull <jocull@delmarsd.com>
2017-06-01 14:23:35 -0400

    Working arbitrarily sized fibonacci generator service with big-integer

--- fib.js
+++ fib.js
@@ -1,24 +1,59 @@
-function fibonacciFlat(n) {
-    if (n <= 1) {
-        return 0;
-    }
+const bigInt = require('big-integer');
+const fibCache = new Map();
 
-    var first = 0, second = 1, fib = 0;
-    for (var i = 2; i < n; i++) {
-        fib = first + second;
-        first = second;
-        second = fib;
-    }
-    return fib;
-}
-
-function fibonacciRecursive(num) {
-  if (num <= 1) return 1;
-
-  return fibonacciRecursive(num - 1) + fibonacciRecursive(num - 2);
-}
-
-module.exports = function (input, callback) {
-    var result = fibonacciRecursive(input);
-    callback(null, result);
+module.exports = function (inputNumStr, callback) {
+  let inputBigInt = bigInt(inputNumStr.toString());
+  // let resultBigInt = fibonacciRecursive(inputBigInt);
+  let resultBigInt = fibonacciGetFromGenerator(inputBigInt);
+  let resultStr = resultBigInt.toString();
+  callback(null, resultStr);
 };
+
+// Example (in Python):
+// https://stackoverflow.com/a/22111492/97964
+// Beware Python operation evaluation ordering:
+// https://stackoverflow.com/questions/8725673/multiple-assignment-and-evaluation-order-in-python
+// def fib(n):
+//   a, b = 0, 1
+//   while n > 0:
+//     a, b = b, a+b
+//     n -= 1
+//   return a
+
+const fibonacciGenerator = (function* () {
+  let a = bigInt.zero;
+  let b = bigInt.one;
+  let n = bigInt.zero;
+
+  // Yield the first number (zero)
+  yield {
+    n: n,
+    fn: a,
+  };
+
+  while (true) {
+    let newA = b;
+    let newB = a.add(b);
+    a = newA;
+    b = newB;
+    n = n.add(bigInt.one);
+    yield {
+      n: n,
+      fn: a,
+    };
+  }
+})();
+
+function fibonacciGetFromGenerator(nBigInt) {
+  let result = fibCache.get(nBigInt.toString());
+  if (result) {
+    return result.fn;
+  }
+
+  do {
+    result = fibonacciGenerator.next().value;
+    fibCache.set(result.n.toString(), result);
+  } while (result.n.lt(nBigInt));
+
+  return result.fn;
+}
--- routes/index.js
+++ routes/index.js
@@ -2,33 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 var workerFarm = require('worker-farm');
-var workers = workerFarm(require.resolve('../fib'));
+var workers = workerFarm({ maxConcurrentWorkers: 1, autoStart: true }, require.resolve('../fib'));
 
 /* GET home page. */
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
 
-router.get('/fib/:fib', function(req, res) {
+router.get('/fib/:fib', function (req, res) {
     var fibN = parseInt(req.params.fib);
     if (fibN == NaN || fibN < 0) {
         res.send(400, "Bad fib value! " + fibN);
     } else {
         new Promise(function (resolve, reject) {
-            workers(fibN, function(err, result) {
+            workers(fibN, function (err, result) {
                 if (err) {
                     reject(err);
                 } else {
                     resolve(result);
                 }
             });
-        }).then(function(result) {
+        }).then(function (result) {
             res.status(200)
-               .send({
-                   result
+                .send({
+                    result
                 });
-        }).catch(function(err) {
+        }).catch(function (err) {
             res.status(500).send({
                 message: err.message,
                 stack: err.stack
