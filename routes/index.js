dd0b1960b82c0b3aaa5a8fbd0dbbe38ff4352d7d
James O'Cull <james.ocull@gmail.com>
2021-09-04 20:44:43 -0400

    Move `workers` to `lib` since there's no workers anymore

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 const bigInt = require('big-integer');
-const { getFibonacci } = require('../workers/fib');
+const { getFibonacci } = require('../lib/fib');
 
 const LIMIT = bigInt('200000');
 
