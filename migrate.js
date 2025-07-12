2a184681133b8e56f984a4ea1e4553411a49a350
James O'Cull <james.ocull@gmail.com>
2025-07-11 22:20:13 -0400

    Rewrite migration to reduce overhead on exist checks

--- migrate.js
+++ migrate.js
@@ -13,6 +13,12 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
+const existQuery = `
+  SELECT count(*) as c
+  FROM fib_cache
+  WHERE n = ?
+`.trim();
+
 const insertQuery = `
   INSERT IGNORE INTO fib_cache (n, a, b, fn)
   VALUES (?, ?, ?, ?)
@@ -36,38 +42,54 @@ async function migrateCache() {
       .map(file => parseInt(file))
       .sort((a, b) => b - a)
       .map(file => file.toString());
-    let fileCounter = 0;
+    const fileCounter = { c: 0 };
     const tasks = files.map(file => async () => {
       try {
-        fileCounter++;
-        const filePath = path.join(cacheDir, file);
-        const content = await fs.readFile(filePath, 'utf-8');
-        const lines = content.trim().split('\n');
-
-        if (lines.length !== 4) {
-          throw new Error(`Invalid file ${file} (expected 4 lines, got ${lines.length})`)
-        }
-
-        const [n, a, b, fn] = lines;
-        const nInt = parseInt(n);
-
-        if (isNaN(nInt)) {
-          console.warn(`Skipping invalid file ${file} (invalid n value: ${n})`);
-          return;
-        }
-
-        const conn = await pool.promise().getConnection();
-        try {
-          const countRows = await conn.query(`SELECT count(*) as c FROM fib_cache WHERE n = ?`, [nInt]);
-          const count = countRows[0][0].c;
-          if (count > 0) {
-            console.log('Exists: n=', nInt, fileCounter, files.length, new Date());
-          } else {
-            await conn.query(insertQuery, [nInt, a, b, fn]);
-            console.log(`Added n=${nInt} from file ${file}`, fileCounter, files.length, new Date());
+        const thisFileCount = fileCounter.c++;
+        // Match query
+        {
+          const nInt = parseInt(file);
+          if (isNaN(nInt)) {
+            throw new Error(`Invalid file ${file} (didn't parse)`);
           }
-        } finally {
-          conn.release();
+          const conn = await pool.promise().getConnection();
+          try {
+            const countRows = await conn.query(existQuery, [nInt]);
+            const count = countRows[0][0].c;
+            if (count > 0) {
+              console.log('Exists: n=', nInt, thisFileCount, files.length, new Date());
+              return;
+            }
+          } finally {
+            conn.release();
+          }
+        }
+
+        // Insert query
+        {
+          const filePath = path.join(cacheDir, file);
+          const lines = (await fs.readFile(filePath, 'utf-8'))
+            .trim()
+            .split('\n');
+
+          if (lines.length !== 4) {
+            throw new Error(`Invalid file ${file} (expected 4 lines, got ${lines.length})`);
+          }
+
+          const [n, a, b, fn] = lines;
+          const nInt = parseInt(n);
+          if (isNaN(nInt)) {
+            console.warn(`Skipping invalid file ${file} (invalid n value: ${n})`);
+            return;
+          }
+
+          const conn = await pool.promise().getConnection();
+          try {
+            await conn.query(insertQuery, [nInt, a, b, fn]);
+            console.log(`Added n=${nInt} from file ${file}`, thisFileCount, files.length, new Date());
+          } finally {
+            conn.release();
+          }
         }
       } catch (err) {
         console.error(`Error processing file ${file}:`, err);
