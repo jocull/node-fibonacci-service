618106df44e3a7bb5b7621c10b56850fe3abc497
James O'Cull <james.ocull@gmail.com>
2025-07-15 22:34:49 -0400

    Some initial experiments with compression
    Does not actually seem to save space when we're already hex encoded
    buffers. Kind of amazed it's not doing... anything? Ratios are worse
    than not trying to compress at all.

--- bench.js
+++ bench.js
@@ -1,65 +1,31 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-function bigIntHexString(bigint) {
-  const hex = bigint.toString(16);
-  const paddedHex = '0x' + (hex.length % 2 ? '0' + hex : hex);
-  return paddedHex;
-}
+const { compress, decompress } = require('@mongodb-js/zstd');
 
-function hexStringToBigint(hexStr) {
-  return BigInt(hexStr);
-}
+const fib = require('./lib/fib');
 
+const cacheDir = path.resolve('.cache');
 (async () => {
   console.log(new Date(), 'Go');
-  const filePath = path.resolve('.cache2/100000000');
-  const lines = (await fs.readFile(filePath, 'utf-8'))
-    .trim()
-    .split('\n');
-  console.log(new Date(), 'Read + Split');
-
-  if (lines.length !== 4) {
-    throw new Error(`Invalid file ${file} (expected 4 lines, got ${lines.length})`);
-  }
-
-  const [n, a, b, fn] = lines;
-  const nInt = parseInt(n);
-  console.log(new Date(), 'Parsed n', n, '@', fn.length);
-  if (isNaN(nInt)) {
-    console.warn(`Skipping invalid file ${file} (invalid n value: ${n})`);
-    return;
-  }
+  const fibN = await fib.readCacheFile(cacheDir, 100000000);
+  console.log(new Date(), 'Read File');
 
   const s1 = Date.now();
-  const fnb = BigInt(fn);
+  const fnb = fib.bigIntToHexBuffer(fibN.fn);
+  const fnbc = await compress(fnb, 3);
   const e1 = Date.now();
-  console.log(new Date(), 'Parsed fnb', e1 - s1);
+  console.log(new Date(), 'Compressed fnbc', fnb.length, 'vs', fnbc.length, 'in', e1 - s1);
+  console.log('Ratio:', (fnbc.length / fnb.length));
 
   const s2 = Date.now();
-  const fns = fnb.toString();
+  const fnbd = await decompress(fnbc);
   const e2 = Date.now();
-  console.log(new Date(), 'Stringified fns', fns.length, e2 - s2);
+  console.log(new Date(), 'Decompressed fnbd', fnbd.length, 'again', 'in', e2 - s2);
 
   const s3 = Date.now();
-  const fnbb = bigIntHexString(fnb);
+  const fnr = fib.bigIntFromHexBuffer(fnbd);
   const e3 = Date.now();
-  console.log(new Date(), 'Buffered fnbb', fnbb.length, e3 - s3);
-  console.log(fnbb.substring(0, 100));
-
-  const s4 = Date.now();
-  const fnbub = hexStringToBigint(fnb);
-  const e4 = Date.now();
-  console.log(new Date(), 'Unbuffered fnbub', fnbb.length, e4 - s4);
-  console.log('Same?', fnb === fnbub);
-
-  const filePath2 = path.resolve('.cache2/100000000x');
-  const lines2 = [
-    n.toString(),
-    bigIntHexString(BigInt(a)),
-    bigIntHexString(BigInt(b)),
-    fnbub,
-  ];
-  await fs.writeFile(filePath2, lines2.join('\n'), 'utf-8');
-  console.log(new Date(), 'Write + Split');
+  console.log(new Date(), 'Restored fnr', 'in', e3 - s3);
+  console.log('Same?', fibN.fn === fnr);
 })();
