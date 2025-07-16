d4f0a3d661fec01277efa3624be9528f36361c5c
James O'Cull <james.ocull@gmail.com>
2025-07-15 22:47:59 -0400

    More compression experiments. Still seems like it doesn't make sense.

--- bench.js
+++ bench.js
@@ -11,9 +11,18 @@ const cacheDir = path.resolve('.cache');
   const fibN = await fib.readCacheFile(cacheDir, 100000000);
   console.log(new Date(), 'Read File');
 
+  const cl = 3;
+
+  const fns = fibN.fn.toString();
+  const fnb = Buffer.from(fns);
+  console.log(new Date(), 'Converted to base10 buffer', fnb.length, 'vs', (await compress(fnb, cl)).length);
+
+  const fns64 = fib.bigIntToBase64String(fibN.fn);
+  const fnb64 = Buffer.from(fns64);
+  console.log(new Date(), 'Converted to base64 buffer', fnb64.length, 'vs', (await compress(fnb64, cl)).length);
+
   const s1 = Date.now();
-  const fnb = fib.bigIntToHexBuffer(fibN.fn);
-  const fnbc = await compress(fnb, 3);
+  const fnbc = await compress(fnb, cl);
   const e1 = Date.now();
   console.log(new Date(), 'Compressed fnbc', fnb.length, 'vs', fnbc.length, 'in', e1 - s1);
   console.log('Ratio:', (fnbc.length / fnb.length));
@@ -24,7 +33,7 @@ const cacheDir = path.resolve('.cache');
   console.log(new Date(), 'Decompressed fnbd', fnbd.length, 'again', 'in', e2 - s2);
 
   const s3 = Date.now();
-  const fnr = fib.bigIntFromHexBuffer(fnbd);
+  const fnr = BigInt(fnbd.toString());
   const e3 = Date.now();
   console.log(new Date(), 'Restored fnr', 'in', e3 - s3);
   console.log('Same?', fibN.fn === fnr);
