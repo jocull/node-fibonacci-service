1c0c28f863030d56ea924b27ec92456eeb293969
James O'Cull <james.ocull@gmail.com>
2025-07-18 10:51:29 -0400

    Raise run target

--- run.js
+++ run.js
@@ -2,5 +2,5 @@ const fs = require('fs').promises;
 const fib = require('./lib/fib');
 
 (async function () {
-    var fibN = await fib.getFibonacci(150_000_000);
+    var fibN = await fib.getFibonacci(200_000_000);
 })();
