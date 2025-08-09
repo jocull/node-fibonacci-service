d0ef65f6792ad1e1c4365c5d7ff280277991d1ea
James O'Cull <james.ocull@gmail.com>
2025-08-08 22:21:14 -0400

    Copied in fast-doubling fib impl from other work. Proof of concept worker fibs.

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 const { Worker } = require('worker_threads');
 
 const worker = new Worker('./lib/worker.js');
-worker.on('message', msg => {
-    console.log('Main:', typeof msg, msg);
-    if (msg === 'bye') {
+worker.on('error', (err) => console.error(err));
+worker.on('exit', (code) => console.warn('Worker exited with code', code));
+
+(async function main() {
+    console.log('Main: begin...');
+    worker.on('message', msg => {
+        // console.log('Main:', typeof msg, msg);
+        console.log('Main:', typeof msg,
+            msg?.op, msg?.n, typeof msg?.result,
+            msg?.result.toString(16).length);
+
+        // Right now, always exit after no matter what.
         worker.postMessage('bye');
-        // worker.unref();
-        setTimeout(() => console.log('ding'), 5000);
-    }
-});
-worker.postMessage('hey');
-worker.postMessage(105n);
-worker.postMessage('bye');
+    });
+    worker.postMessage({
+        op: 'fib',
+        n: 100_000,
+    });
+    worker.postMessage({
+        op: 'fib',
+        n: 1_000_000,
+    });
+    worker.postMessage({
+        op: 'fib',
+        n: 100_000_000,
+    });
+})();
