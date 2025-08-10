7002152408f4296b4def7f0204608796bfd3cec2
James O'Cull <james.ocull@gmail.com>
2025-08-09 23:13:54 -0400

    Yay, a basic worker pool!
    Not a great implementation, but does work in short bursts

--- main.js
+++ main.js
@@ -1,38 +1,58 @@
+const os = require('os');
 const { Worker } = require('worker_threads');
 
-const worker = new Worker('./lib/worker.js');
-worker.on('error', (err) => console.error(err));
-worker.on('exit', (code) => console.warn('Worker exited with code', code));
+const workers = new Array(os.cpus().length).fill(0)
+    .map(() => new Worker('./lib/worker.js'))
+    .map(worker => {
+        worker.on('error', (err) => console.error(err));
+        worker.on('exit', (code) => console.warn('Worker exited with code', code));
+        worker.on('message', msg => {
+            // console.log('Main:', typeof msg, msg);
+            console.log('Main:', typeof msg,
+                msg?.id,
+                msg?.op, msg?.n, typeof msg?.result,
+                msg?.result.toString(16).length);
+        });
+        return worker;
+    });
+
+let workerIdx = 0;
+function worker() {
+    const w = workers[workerIdx];
+    workerIdx++;
+    if (workerIdx > workers.length - 1) {
+        workerIdx = 0;
+    }
+    return w;
+}
 
 (async function main() {
+    let msgId = 0;
     console.log('Main: begin...');
-    worker.on('message', msg => {
-        // console.log('Main:', typeof msg, msg);
-        console.log('Main:', typeof msg,
-            msg?.op, msg?.n, typeof msg?.result,
-            msg?.result.toString(16).length);
-
-        // Right now, always exit after no matter what.
-        worker.postMessage('bye');
+    const workload = [
+        100_000_000,
+        100_000,
+        1_000_000,
+        200_000_000,
+        1000,
+    ];
+    const promises = workload.map(thisN => {
+        const id = ++msgId
+        return new Promise(resolve => {
+            const w = worker();
+            w.postMessage({
+                id: id,
+                op: 'fib',
+                n: thisN,
+            });
+            // TODO: Bad, will always keep function attached!
+            w.on('message', msg => {
+                if (msg.id == id) {
+                    resolve(msg);
+                }
+            });
+        });
     });
-    worker.postMessage({
-        op: 'fib',
-        n: 100_000_000,
-    });
-    worker.postMessage({
-        op: 'fib',
-        n: 100_000,
-    });
-    worker.postMessage({
-        op: 'fib',
-        n: 1_000_000,
-    });
-    worker.postMessage({
-        op: 'fib',
-        n: 200_000_000,
-    });
-    worker.postMessage({
-        op: 'fib',
-        n: 1000,
-    });
+    await Promise.all(promises);
+    workers.forEach(w => w.postMessage('bye'));
 })();
